fbf6ab66c7bc890dffa4b01e4a08b88cfbe1f78e
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-01-22 17:06:43 -0300

    chore: configure jest

---
+++ src/tests/Utils.test.ts
@@ -0,0 +1,8 @@
+import { Utils } from '../app/Utils'
+
+describe('Utils', () => {
+  it('works', () => {
+      const test = Utils.toUppercase('Some value')
+      console.log('This works')
+  })
+})
